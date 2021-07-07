b7fe7af72f047875a57c73caae698731cbfac268
unknown <fabien.sabatie@live.fr>
2021-07-08 00:07:48 +0200

    custom indicators

--- src/adapters/factories/indicators/rsi.indicator-method.ts
+++ src/adapters/factories/indicators/rsi.indicator-method.ts
@@ -2,27 +2,28 @@ import { Candle, RSIIndicatorOutput, RSIIndicatorParameters, RSIMethod, RSIMetho
 import { Indicators } from "../indicators.factory";
 
 const RSI:RSIMethodBuilder = (parameters: RSIIndicatorParameters) => {
-  const indicator = new Indicators();
-  const gain = indicator.averageGain({ period: parameters.period });
-  const loss = indicator.averageLoss({ period: parameters.period });
-  let RS = 0, currentRSI = 0;
+  // const indicator = new Indicators();
+  // const gain = indicator.averageGain({ period: parameters.period });
+  // const loss = indicator.averageLoss({ period: parameters.period });
+  // let RS = 0, currentRSI = 0;
   return ({ candle }: RSIMethodParameters) => {
-    gain.generate(candle);
-    loss.generate(candle);
-    if ((gain.lastValue !== null) && (loss.lastValue !== null)) {
-        if (loss.lastValue === 0) {
-            currentRSI = 100;
-        }
-        else if (gain.lastValue === 0) {
-            currentRSI = 0;
-        }
-        else {
-            RS = (gain.lastValue as number) / (loss.lastValue as number);
-            RS = isNaN(RS) ? 0 : RS;
-            currentRSI = parseFloat((100 - (100 / (1 + RS))).toFixed(2));
-        }
-    }
-    return currentRSI;
+    // gain.generate(candle);
+    // loss.generate(candle);
+    // if ((gain.lastValue !== null) && (loss.lastValue !== null)) {
+    //     if (loss.lastValue === 0) {
+    //         currentRSI = 100;
+    //     }
+    //     else if (gain.lastValue === 0) {
+    //         currentRSI = 0;
+    //     }
+    //     else {
+    //         RS = (gain.lastValue as number) / (loss.lastValue as number);
+    //         RS = isNaN(RS) ? 0 : RS;
+    //         currentRSI = parseFloat((100 - (100 / (1 + RS))).toFixed(2));
+    //     }
+    // }
+    // return currentRSI;
+    return 1
   }
 }
 
